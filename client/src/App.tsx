ddfd335fe3b712020d4aa64c79b0d411d3bf23b5
Узаков Никита <nuzakov@htc-cs.ru>
2021-05-14 20:22:18 +0400

    init socket on server and client(demo)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
+
+import React, { useEffect } from 'react';
+import {io} from 'socket.io-client';
 import './App.css';
 
 const App = () => {
+    useEffect(() => {
+        const socket = io('http://localhost:1000');
+        socket.on('connect', () => {
+            socket.send('hello')
+        })
+    }, [])
     return (
         <div className="app">
         </div>
--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, { cors: { origin: '*' } });
+const cors = require('cors');
 
+const PORT = process.env.PORT || 1000;
 
+const rooms = new Map();
+app.use(cors());
+app.get('/rooms', (req, res) => {
+    rooms.set();
+    res.json(rooms);
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socket.on('message', (data) => {
+        console.log(data);
+    });
+});
+
+server.listen(PORT, (err) => {
+    if (err) {
+        throw new Error(err);
+    }
+    console.log(`server has been started on PORT ${PORT}`);
+});
