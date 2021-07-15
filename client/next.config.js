35c6683fb594fd87f05ed5d47d7f4dae4faa854f
Узаков Никита <nuzakov@htc-cs.ru>
2021-07-15 11:02:11 +0400

    added csp

---
+++ client/next.config.js
@@ -0,0 +1,15 @@
+const securityHeaders = [{
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; child-src 'none'; style-src 'unsafe-inline' https://fonts.googleapis.com http://localhost:3000; font-src https://fonts.gstatic.com; connect-src ws://localhost:1000/ http://localhost:1000/socket.io/ http://localhost:3000/"
+}];
+module.exports = {
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
+    },
+};
