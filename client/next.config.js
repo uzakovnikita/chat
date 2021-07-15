d097f09efe476eae0506bbbf781e258e60605f98
Узаков Никита <nuzakov@htc-cs.ru>
2021-07-15 11:02:11 +0400

    Merge branch 'server' of https://github.com/uzakovnikita/chat into server

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 const securityHeaders = [{
     key: 'Content-Security-Policy',
-    value: "default-src 'self'; child-src 'none'; style-src 'unsafe-inline' https://fonts.googleapis.com http://localhost:3000; font-src https://fonts.gstatic.com; connect-src ws://localhost:1000/ http://localhost:1000/socket.io/ http://localhost:3000/"
+    value: "default-src 'self'; child-src 'none'; style-src 'unsafe-inline' https://fonts.googleapis.com http://localhost:3000; font-src https://fonts.gstatic.com; connect-src ws://localhost:1000/ http://localhost:1000/ http://localhost:3000/"
 }];
 module.exports = {
     async headers() {
