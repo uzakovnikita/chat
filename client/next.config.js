b9d61aa1a2a0b3e9325318cac4147199b1a8e042
Узаков Никита <nuzakov@htc-cs.ru>
2021-09-02 13:12:03 +0400

    In progress

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
-const securityHeaders = [{
-    key: 'Content-Security-Policy',
-    value: "default-src 'self'; child-src 'none'; style-src 'unsafe-inline' https://fonts.googleapis.com http://localhost:3000; font-src https://fonts.gstatic.com; connect-src ws://localhost:1000/ http://localhost:1000/ http://localhost:3000/"
-}];
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache')
+
+const securityHeaders = [
+    {
+        key: 'Content-Security-Policy',
+        value:
+            "default-src 'self'; child-src 'none'; style-src 'unsafe-inline' https://fonts.googleapis.com http://localhost:3000; font-src https://fonts.gstatic.com; connect-src ws://localhost:1000/ http://localhost:1000/ http://localhost:3000/",
+    },
+];
 module.exports = {
     async headers() {
         return [
@@ -13,3 +19,10 @@ module.exports = {
         ];
     },
 };
+module.exports = withPWA({
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+        dynamicStartUrlRedirect : '/auth'
+    },
+});
